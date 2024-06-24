187f4f3998c3733c6a98d3e935d66cf40cde1268
Afrokk <afrasiyabkhan379@gmail.com>
2024-06-24 00:50:28 -0400

    feat(navbar): increase font size

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,20 +3,24 @@ import LocaleSwitcher from "@components/locale-switch/locale-switch";
 import { cva } from "class-variance-authority";
 import { getDictionary } from "@utils/getDictionary";
 import type { Locale } from "@lib/i18n-config";
+import { NotoSans } from "@app/fonts";
 
 export default async function Navbar({ lang }: { lang: Locale }) {
   const dictionary = await getDictionary(lang);
 
   const navigationMenuButtonStyles = cva(
-    "group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
+    "group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
   );
 
   return (
     <nav className="fixed inset-x-0 top-0 z-50 bg-white shadow-sm dark:bg-gray-950/90">
       <div className="w-full max-w-7xl mx-auto px-4">
         <div className="flex justify-between h-14 items-center">
-          <Link href="#" className="flex items-center">
-            {dictionary.nav.brand}
+          <Link
+            href="#"
+            className={`${NotoSans.className} flex items-center font-bold text-2xl`}
+          >
+            Allied<span className="font-normal">Schools</span>
           </Link>
           <nav className="hidden md:flex gap-10">
             <Link href="/" className={navigationMenuButtonStyles()}>
