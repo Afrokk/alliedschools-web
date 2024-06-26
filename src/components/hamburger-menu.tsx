2ba428a84f37d390e860db9585bcf31b6f8d7df8
Afrokk <afrasiyabkhan379@gmail.com>
2024-06-25 23:59:26 -0400

    feat(navbar): add hamburger menu

---
+++ src/components/hamburger-menu.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import LocaleSwitcher from "@components/locale-switch/locale-switch";
+
+export default function HamburgerMenu({
+  navigationMenuButtonStyles,
+  dictionary,
+}) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <>
+      <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+        <svg
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h16m-7 6h7"
+          />
+        </svg>
+      </button>
+      <nav
+        className={`absolute top-full left-0 w-full bg-white shadow-md md:shadow-none md:bg-transparent md:relative md:w-auto md:flex gap-10 ${
+          isMenuOpen ? "block" : "hidden"
+        }`}
+      >
+        <Link href="/" className={navigationMenuButtonStyles()}>
+          {dictionary.nav.home}
+        </Link>
+        <Link href="/about" className={navigationMenuButtonStyles()}>
+          {dictionary.nav.about}
+        </Link>
+        <Link href="/contact" className={navigationMenuButtonStyles()}>
+          {dictionary.nav.contact}
+        </Link>
+        <div className="flex items-center gap-4 p-4 md:p-0">
+          <LocaleSwitcher />
+        </div>
+      </nav>
+    </>
+  );
+}
