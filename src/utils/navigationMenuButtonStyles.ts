0492837eabfef5cfa0259a0252db8d21ec1973f5
Afrokk <afrasiyabkhan379@gmail.com>
2024-07-14 03:44:29 -0400

    feat(utils): add mobile variant for nav menu links

--- src/utils/navigationMenuButtonStyles.ts
+++ src/utils/navigationMenuButtonStyles.ts
@@ -1,5 +1,21 @@
 import { cva } from "class-variance-authority";
 
 export const navigationMenuButtonStyles = cva(
-  "group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50 hover:text-alliedBlue"
+  "group h-10 items-center justify-center rounded-md px-4 py-2 font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50 hover:text-alliedBlue",
+  {
+    variants: {
+      size: {
+        base: "inline-flex w-max bg-background",
+        mobile: "flex w-1/4 bg-gray-50 mx-4",
+      },
+      shape: {
+        rounded: "rounded-md",
+        circle: "rounded-full",
+      },
+    },
+    defaultVariants: {
+      size: "base",
+      shape: "rounded",
+    },
+  }
 );
