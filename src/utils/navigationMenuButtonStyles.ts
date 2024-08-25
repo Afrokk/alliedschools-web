4494883bf35b881bcfa95363c14c8c4c48b3ccb3
Afrokk <afrasiyabkhan379@gmail.com>
2024-08-25 08:17:19 -0400

    fix(nav): change button hover styles

--- src/utils/navigationMenuButtonStyles.ts
+++ src/utils/navigationMenuButtonStyles.ts
@@ -1,7 +1,7 @@
 import { cva } from "class-variance-authority";
 
 export const navigationMenuButtonStyles = cva(
-  "group h-10 items-center justify-center rounded-md px-4 py-2 font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50 hover:text-alliedBlue",
+  "group h-10 items-center justify-center rounded-md px-4 py-2 font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50 hover:text-alliedDarkBlue",
   {
     variants: {
       size: {
